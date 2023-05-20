0eb61acb754f6a9338752185b7436d600889bfd2
Kevin Jay <kjayh@unal.edu.co>
2023-05-20 01:16:20 -0500

    Login created in chat-app 📢

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate  from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia().use(piniaPluginPersistedstate))
 app.use(router)
 
 app.mount('#app')
