05e433e66795db2897a0077834ce06620504d32c
Kevin Jay <kjayh@unal.edu.co>
2023-05-26 22:40:47 -0500

    Add login modal

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,33 +1,89 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue'
 import { useAuth } from '@/stores/auth'
-import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router'
 
-const store = useAuth();
-const email = ref('');
-const password = ref('');
-const router = useRouter();
+const store = useAuth()
+const email = ref('')
+const password = ref('')
+const router = useRouter()
+const error = ref('')
 
 async function login() {
   await store.login(email.value, password.value)
   router.push('/')
 }
+
+async function closeModal() {
+  error.value = ''
+  router.go(0)
+}
+watchEffect(() => {
+  error.value = store.errorMsg
+})
 </script>
 <template>
   <form @submit.prevent="login()" class="bg-[#2f2f2f] p-8 rounded-xl">
     <div class="mb-6">
       <label for="email" class="block mb-2 text-sm font-medium">Your email</label>
-      <input type="email" id="email" v-model="email"
+      <input
+        type="email"
+        id="email"
+        v-model="email"
         class="border-b border-gray-600 block w-full py-1.5 bg-transparent dark:placeholder-gray-400 focus:outline-none focus:border-[#42b883]"
-        placeholder="example@mail.com" required>
+        placeholder="example@mail.com"
+        required
+      />
     </div>
     <div class="mb-6">
-      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-      <input type="password" id="password" v-model="password"
+      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Your password</label
+      >
+      <input
+        type="password"
+        id="password"
+        v-model="password"
         class="bg-transparent border-b border-gray-600 block w-full py-1.5 focus:outline-none focus:border-[#42b883]"
-        required>
+        required
+      />
     </div>
     <button
-      class="text-white bg-[#42b883] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center focus:bg-green-700">Submit</button>
+      class="text-white bg-[#42b883] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center focus:bg-green-700"
+    >
+      Submit
+    </button>
   </form>
+  <div v-if="error" class="fixed z-10 bg-gray-600 px-5 pb-14 pt-5 rounded-lg">
+    <button @click="closeModal" class="flex w-full justify-end">
+      <svg
+        aria-hidden="true"
+        class="w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+    <svg
+      aria-hidden="true"
+      class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      ></path>
+    </svg>
+    <p class="text-3xl pt-5">{{ error }}</p>
+  </div>
 </template>
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,56 +1,71 @@
-import { defineStore } from "pinia";
-import axios from "@/utils/axios";
-import { ref, type Ref } from "vue";
-import type IUser from "@/interfaces/IUser";
+import { defineStore } from 'pinia'
+import axios from '@/utils/axios'
+import { ref, type Ref } from 'vue'
+import type IUser from '@/interfaces/IUser'
 
-export const useAuth = defineStore('auth', () => {
-  const user: Ref<IUser | null> = ref(null);
-  const token = ref('');
+export const useAuth = defineStore(
+  'auth',
+  () => {
+    const user: Ref<IUser | null> = ref(null)
+    const token = ref('')
+    const errorMsg = ref('')
 
-  async function login(email: string, password: string) {
-    await axios.post('/api/login', {
-      email: email,
-      password: password
-    }).then(res => {
-      token.value = res.data.token
-      user.value = res.data.user
-    })
-  }
+    async function login(email: string, password: string) {
+      await axios
+        .post('/api/login', {
+          email: email,
+          password: password
+        })
+        .then((res) => {
+          token.value = res.data.token
+          user.value = res.data.user
+        })
+        .catch(function (error) {
+          errorMsg.value = error.response.data.error
+        })
+    }
 
-  async function register(name: string, email: string, password: string) {
-    await axios.post('/api/register', {
-      name: name,
-      email: email,
-      password: password
-    }).then(res => {
-      token.value = res.data.token;
-      user.value = res.data.user;
-    })
-  }
+    async function register(name: string, email: string, password: string) {
+      await axios
+        .post('/api/register', {
+          name: name,
+          email: email,
+          password: password
+        })
+        .then((res) => {
+          token.value = res.data.token
+          user.value = res.data.user
+        })
+    }
 
-  async function logout() {
-    await axios.get('/sanctum/csrf-cookie').then(() => {
-      axios.post('/api/logout', {}, {
-        headers: {
-          "Content-type": "application/json",
-          "Authorization": `Bearer ${token.value}`,
-        }
-      }).then(res => {
-        console.log(res.data)
+    async function logout() {
+      await axios.get('/sanctum/csrf-cookie').then(() => {
+        axios
+          .post(
+            '/api/logout',
+            {},
+            {
+              headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${token.value}`
+              }
+            }
+          )
+          .then((res) => {
+            console.log(res.data)
+          })
       })
-    })
 
-    localStorage.clear();
-    token.value = '';
-    user.value = null;
-  }
+      localStorage.clear()
+      token.value = ''
+      user.value = null
+    }
 
-  return { user, token, login, logout,  register }
-
-},
+    return { user, token, login, logout, register, errorMsg }
+  },
   {
     persist: {
       paths: ['user', 'token']
     }
-
-  })
+  }
+)
